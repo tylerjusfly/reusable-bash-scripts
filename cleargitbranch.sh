39ccb36d730a151a523b503ba7b89e793a6fba76
tylerjusfly <tylerjusfly1@gmail.com>
2024-04-29 09:48:54 +0100

    feat: added branches to ignore

--- cleargitbranch.sh
+++ cleargitbranch.sh
@@ -4,6 +4,10 @@
 read -p "Enter the name of the branch to check out (default: staging): " branch_name
 branch_name=${branch_name:-staging}
 
+# Prompt user for branches to ignore
+read -p "Enter the name of the branches to ignore.: " ignore_branches 
+ignore_branches=${ignore_branches}
+
 # Check if Git is installed
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Please install Git and run the script again."
@@ -20,7 +24,22 @@ else
 fi
 
 # Check if there are any local branches (excluding the specified branch)
-branches=$(git branch | grep -v "$branch_name")
+# branches=$(git branch | grep -v "$branch_name")
+
+# Get all local branches
+branches=$(git branch)
+
+# Filter out the specified branch and branches in the ignore list
+for branch in $ignore_branches; do
+  branches=$(echo "$branches" | grep -v "\b$branch\b")
+done
+# Filter out the checkout branch and branches in the ignore list
+# for branch in "$branches"; do
+#   if [[ "$branch" == "$ignore_branches" || "$branch" == "$branch_name" ]]; then
+#     continue  # Skip checkout branch and branches in the ignore list
+#   fi
+#   branches=$(echo "$branches" | grep -v "\b$branch\b")
+# done
 
 if [ -z "$branches" ]; then
     echo "No local branches to remove."
@@ -28,7 +47,7 @@ if [ -z "$branches" ]; then
 fi
 
 # Confirm with the user before removing branches
-read -p "This will remove all local branches except '$branch_name'. Are you sure? (y/n): " confirm
+read -p "This will remove all local branches except '$branch_name' and ignore '$ignore_branches'. Are you sure? (y/n): " confirm
 
 if [ "$confirm" != "y" ]; then
     echo "Operation aborted."
@@ -37,5 +56,7 @@ fi
 
 # Remove local branches (excluding the specified branch)
 echo "$branches" | xargs git branch -D
+
+# echo $ignore_branches
 
 echo "Local branches (except '$branch_name') removed successfully."
